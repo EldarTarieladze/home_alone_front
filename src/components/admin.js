44e762189fcb29991544fe443b02671fca053b1e
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2021-12-29 18:10:34 +0400

    next3

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+// import React, { useState, useEffect, useContext } from "react";
 
 export default function Admin() {
   return (
